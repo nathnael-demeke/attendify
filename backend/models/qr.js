d588fcb1f2c5b99d5b0bd9a188931438fe8ab253
hoppxi <hoppxii@gmail.com>
2025-02-03 11:21:19 +0300

    qrcode images saving file path

--- backend/models/qr.js
+++ backend/models/qr.js
@@ -2,7 +2,7 @@ import QRCodeReader from 'qrcode-reader';
 import qrcode from 'qrcode';
 class QR {
     static async generateQRCode(data,filename) {
-        var qrCodePath = `/QRCodes/${filename}.png`
+        var qrCodePath = `/images/qrcodes/${filename}.png`
         var qrcodeFilePath = `../public${qrCodePath}`
         var qrFile = await qrcode.toFile(qrcodeFilePath,data)
         return qrCodePath
